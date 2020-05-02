d662253e766482fdca90ac331654bd7d1aeee76b
muco <mucorolle@gmail.com>
2020-05-02 08:46:10 +0200

    style: Add navigations on the header.

--- src/app/components/app-header/AppHeader.tsx
+++ src/app/components/app-header/AppHeader.tsx
@@ -1,37 +1,31 @@
 import React from "react";
-import styled from "styled-components";
+import { FaPlus } from "react-icons/fa";
 import { Logo } from "app/components";
-
-const StyledAppHeader = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-    padding: 0 40px;
-    height: 70px;
-
-    .logo {
-        line-height: 70px;
-    }
-
-    nav ul {
-        display: flex;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
-`;
+import { StyledAppHeader } from "./app-header.style";
+import { Whisper, Popover } from "rsuite";
 
 export const AppHeader = () => {
+    const speaker = (
+        <Popover>
+            <p>add project</p>
+        </Popover>
+    );
     return (
         <StyledAppHeader>
             <Logo className="logo" />
             <nav>
                 <ul>
-                    <li>search field...</li>
-                    <li>add project button</li>
-                    <li>avatar</li>
+                    <li className="addButton">
+                        <Whisper
+                            placement="bottom"
+                            trigger="hover"
+                            speaker={speaker}
+                        >
+                            <FaPlus />
+                        </Whisper>
+                    </li>
+
+                    <li className="avatar">MU</li>
                 </ul>
             </nav>
         </StyledAppHeader>
---
+++ src/app/components/app-header/app-header.style.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { color } from "utils";
+
+export const StyledAppHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    padding: 0 40px;
+    height: 70px;
+
+    .logo {
+        line-height: 70px;
+    }
+
+    nav ul {
+        display: flex;
+        align-items: center;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    nav ul .addButton {
+        margin-right: 30px;
+        :hover {
+            cursor: pointer;
+        }
+    }
+
+    nav ul .avatar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: 800;
+        width: 50px;
+        height: 50px;
+        border-radius: 50px;
+        border: 2px solid ${color("body2")};
+
+        :hover {
+            cursor: pointer;
+        }
+    }
+`;
